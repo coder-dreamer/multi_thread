93df0e7b3c133f5b2fccc5f2f8afd7c1ed8bea1b
李海鹏 <531372791@qq.com>
2021-03-13 15:48:59 +0800

    multi_thread sleep方法 可以使用 interrupt 方法打断正在睡眠的线程，这时 sleep 方法会抛出 InterruptedException

--- src/main/java/com/lhp/thread/part1/SleepInterrupt.java
+++ src/main/java/com/lhp/thread/part1/SleepInterrupt.java
@@ -5,30 +5,29 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * @author 53137
  */
-@Slf4j(topic = "c.Sleep")
-public class Sleep {
+@Slf4j(topic = "c.SleepInterrupt")
+public class SleepInterrupt {
     public static void main(String[] args) {
         Thread thread = new Thread("t1") {
             @Override
             public void run() {
                 try {
+                    log.debug("sleep");
                     Thread.sleep(2000);
                 } catch (InterruptedException e) {
+                    log.debug("Interrupted");
                     e.printStackTrace();
                 }
             }
         };
         //线程启动
         thread.start();
-        //获取t1线程状态为RUNNABLE，此时t1未执行run方法中的sleep方法
-        log.debug("threadName:{};status:{}", thread.getName(), thread.getState());
-        //主线程休眠
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        //获取t1线程状态为TIMED_WAITING，此时t1执行run方法中的sleep方法
-        log.debug("threadName:{};status:{}", thread.getName(), thread.getState());
+        log.debug("开始打断");
+        thread.interrupt();
     }
 }
---
+++ src/main/java/com/lhp/thread/part1/SleepStatus.java
@@ -0,0 +1,34 @@
+package com.lhp.thread.part1;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 53137
+ */
+@Slf4j(topic = "c.SleepStatus")
+public class SleepStatus {
+    public static void main(String[] args) {
+        Thread thread = new Thread("t1") {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        //线程启动
+        thread.start();
+        //获取t1线程状态为RUNNABLE，此时t1未执行run方法中的sleep方法
+        log.debug("threadName:{};status:{}", thread.getName(), thread.getState());
+        //主线程休眠
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //获取t1线程状态为TIMED_WAITING，此时t1执行run方法中的sleep方法
+        log.debug("threadName:{};status:{}", thread.getName(), thread.getState());
+    }
+}
